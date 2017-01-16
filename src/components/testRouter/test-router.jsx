1e243453a92f18de1711d144ed42bb243064a836
Devin Osorio <me@devinosor.io>
2017-01-16 09:54:22 -0800

    adds content for test router component

--- src/components/testRouter/test-router.jsx
+++ src/components/testRouter/test-router.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const TestRouter = () => (
   <div className="container text-center">
     <h1>This is a test page for react-router v4</h1>
+    <p>{"The url has changed and a full refresh wasn't required. Welcome to the wonderful world of single page applications"}</p>
   </div>
 );
 
