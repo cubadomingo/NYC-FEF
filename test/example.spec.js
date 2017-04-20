98803623e8f54c054ec094fb8f3edf962a33ae68
Devin Osorio <me@devinosor.io>
2017-04-20 16:45:46 -0700

    adds testing with enzyme, mocha, and chai

---
+++ test/example.spec.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import {expect} from 'chai';
+
+describe('Example', function () {
+  it('should have an image to display the gravatar', function () {
+
+  });
+
+  it('should have props for email and src', function () {
+
+  });
+});
