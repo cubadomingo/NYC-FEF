0b994d8f04c319eab1be6d040d26035c7544d2f3
Devin Osorio <me@devinosor.io>
2017-04-27 13:17:17 -0700

    Configures testing environment and linting as well

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,13 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
+    mocha: true,
     es6: true,
     node: true,
   },
   extends: 'airbnb',
   rules: {
+    'import/no-extraneous-dependencies': [2, { devDependencies: true }],
     'react/prop-types': 0,
     'max-len': 0,
   },
