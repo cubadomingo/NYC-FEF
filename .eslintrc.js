5bc1f70efe09e366a4b2a5a7cf017cacb1250384
Devin Osorio <me@devinosor.io>
2017-05-01 14:17:52 -0700

    cleans up setup.js for tests and removes coverage

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,7 @@ module.exports = {
     'max-len': 0,
     'import/no-named-as-default': 0,
     'jsx-a11y/no-static-element-interactions': 0,
+    'import/no-unresolved': 0,
+    'no-underscore-dangle': 0,
   },
 };
--- test/setup.js
+++ test/setup.js
@@ -1,11 +1,25 @@
-import requireHacker from 'require-hacker';
-import mockCSSModules from 'mock-css-modules';
+import Module from 'module';
 
-mockCSSModules.register(['.scss']);
+/* eslint no-unused-vars: 0 */
+/* eslint no-param-reassign: 0 */
+/* eslint no-return-assign: 0 */
 
-requireHacker.hook('png', () => 'module.exports = ""');
-requireHacker.hook('svg', () => 'module.exports = ""');
-requireHacker.hook('jpg', () => 'module.exports = ""');
+// sets require handler for .scss, .png, .svg, .jpg files */
+
+Module._extensions['.scss'] = (module, filename) => {
+  module.exports = new Proxy({}, {
+    get: (target, property) => {
+      if (property === '__esModule') return false;
+      return property;
+    },
+  });
+};
+
+Module._extensions['.png'] = () => module.exports = '';
+Module._extensions['.svg'] = () => module.exports = '';
+Module._extensions['.jpg'] = () => module.exports = '';
+
+// sets dom with jsdom
 
 const jsdom = require('jsdom').jsdom;
 
