9ddafe8a2afc2cf56a9f9f87c3c6ec51829eae5a
Devin Osorio <me@devinosor.io>
2017-05-31 23:16:13 -0700

    adds localStorage mock to test setup

--- test/setup.js
+++ test/setup.js
@@ -19,6 +19,25 @@ Module._extensions['.png'] = () => module.exports = '';
 Module._extensions['.svg'] = () => module.exports = '';
 Module._extensions['.jpg'] = () => module.exports = '';
 
+// sets localStorage mock
+const storageMock = () => {
+  const storage = {};
+
+  return {
+    setItem(key, value) {
+      storage[key] = value || '';
+    },
+    getItem(key) {
+      return key in storage ? storage[key] : null;
+    },
+    removeItem(key) {
+      delete storage[key];
+    },
+  };
+};
+
+global.localStorage = storageMock();
+
 // sets dom with jsdom
 
 const jsdom = require('jsdom').jsdom;
@@ -27,6 +46,7 @@ const exposedProperties = ['window', 'navigator', 'document'];
 
 global.document = jsdom('');
 global.window = document.defaultView;
+global.window.localStorage = global.localStorage;
 Object.keys(document.defaultView).forEach((property) => {
   if (typeof global[property] === 'undefined') {
     exposedProperties.push(property);
