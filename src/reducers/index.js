e6be7255de7a16bb782a1cb16c04ac25f4519966
Devin Osorio <me@devinosor.io>
2017-05-31 23:16:50 -0700

    remove app reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-import appReducer from 'reducers/app';
 import authenticateReducer from 'reducers/authentication';
 
 const rootReducer = combineReducers({
-  app: appReducer,
   form: formReducer,
   authenticate: authenticateReducer,
 });
