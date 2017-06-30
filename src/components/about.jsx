b8632d41ee84019993959f7f65f21fd05a55e7f7
Devin Osorio <me@devinosor.io>
2017-06-29 17:14:37 -0700

    styles about component

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -10,7 +10,7 @@ import styles from 'styles/about.scss';
 const About = () => (
   <div>
     <div styleName="background" />
-    <h1 styleName="main-text" className="text-center">WHAT WE{'\''}RE ABOUT</h1>
+    <h1 styleName="main-text" className="text-center">ABOUT US</h1>
     <Mission />
     <Philosophy />
     <FAQ />
